f658d6d8abbda6c19506122e46a7139c2e9e83c8
ChaitanyaDasari <saichaitanya52@gmail.com>
2019-12-05 03:40:14 -0800

    added profile

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Main from "./Main";
+
 function App() {
   return (
     <div className="App">
---
+++ src/Main.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import { Route } from "react-router-dom";
+
+
+
+class Main extends Component {
+  state = {};
+  render() {
+    return (
+      <div className="container">
+        {/* <Route path="/" exact component={Landing} />
+        <Route path="/signin" component={SignIn} />
+        <Route path="/signup" component={SignUp} /> */}
+      </div>
+    );
+  }
+}
+
+export default Main;
